90fe1e4fcde139340f6eefbe91b0d5aeaa009f89
dan L <danlangford@gmail.com>
2011-12-27 03:56:32 +0000

    mongoose & mongohq implemented

--- index.js
+++ index.js
@@ -1,61 +1,39 @@
 // environment specific constants
-var IPADDY = '0.0.0.0';
-var PORT = process.env.PORT;
+var IPADDY = '0.0.0.0', PORT = process.env.PORT;
 
 // requires (e.g. imports)
-require('sugar')
-var app = require('express').createServer();
-
-
-// test data
-var d = {
-    groups: [{
-        id: 901,
-        name: 'the aardvarks'
-    }, {
-        id: 902,
-        name: 'the bugz'
-    }, {
-        id: 903,
-        name: 'the cloggers'
-    }],
-    users: [{
-        id: 101,
-        name: 'Al Anderson'
-    }, {
-        id: 102,
-        name: 'Berry Benoit'
-    }, {
-        id: 103,
-        name: 'Cory Copper'
-    }]
-};
+require('sugar');
+var app = require('express').createServer(),
+    mon = require('./datasource').mon;
 
 // setup
 app.enable("jsonp callback");
 app.listen(PORT);
 
-
 app.get("/groups/", function(req, res) {
-    res.json(d.groups);
+    mon.Group.find({},function(err,docs){
+        res.json(docs);
+    });
 });
 
-app.get("/groups/:id", function(req, res) {
-    var group = d.groups.find(function(n) {
-        return n.id == req.params.id;
+app.get("/groups/:name", function(req, res) {
+    mon.Group.findOne({name:req.params.name},function(err,doc){
+        doc ?
+            res.json(doc) :
+            res.json('group not found',404) ;
     });
-    group = group || { error: 'group not found' };
-    res.json(group);
 });
 
 app.get("/users/", function(req, res) {
-    res.json(d.users);
+    mon.User.find({},function(err,docs){
+        res.json(docs);
+    });
 });
 
-app.get("/users/:id", function(req, res) {
-    var user = d.users.find(function(n) {
-        return n.id == req.params.id;
+app.get("/users/:name", function(req, res) {
+    mon.User.findOne({name:req.params.name},function(err,doc){
+        doc ?
+            res.json(doc) :
+            res.json('user not found',404) ;
     });
-    user = user || { error: 'user not found' };
-    res.json(user);
 });
